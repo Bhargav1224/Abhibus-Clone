16e91fb138f4154e5b9b264c28b9773cec8b39e2
makkardeepak25 <makkardeepak25@gmail.com>
2021-07-24 03:09:59 +0530

    Footer half done

--- abhibus-clone/Front-end/src/Components/Footer/Footer.styled.js
+++ abhibus-clone/Front-end/src/Components/Footer/Footer.styled.js
@@ -1,26 +1,46 @@
-import styled from "styled-components"
+import styled from "styled-components";
 
 export const Circle = styled.div`
-border: 2px solid black;
-border-radius:10px;
-width:25px;
-height:25px;
-margin-left: 10px;
-
-`
+  border: 2px solid black;
+  border-radius: 10px;
+  width: 25px;
+  height: 25px;
+  margin-left: 10px;
+`;
 export const FlexDiv = styled.div`
-width:33%;
-margin: auto;
-display: flex;
-justify-content: space-evenly;
-margin-bottom: 1.5rem!important;
-`
+  width: 33%;
+  margin: auto;
+  display: flex;
+  justify-content: space-evenly;
+  margin-bottom: 1.5rem !important;
+`;
 export const Social = styled.div`
-font-size:20px;
-`
+  font-size: 20px;
+`;
 export const FootCont = styled.div`
-background-color:#e5e5e5;
-width:100%;
-padding: 10px 20px;
-min-height: 300px;
-`
+  background-color: #e5e5e5;
+  width: 100%;
+  padding: 20px 10px;
+`;
+export const GridItems = styled.div`
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  gap: 0.5%;
+`;
+export const Head1 = styled.div`
+  font-size: 15px !important;
+  color: #212529;
+  font-weight: 600;
+  margin-left: 20px;
+`;
+export const Li = styled.li`
+  cursor: pointer;
+  font-size: 14px;
+  font-weight: 300;
+  line-height: 1.5;
+  margin-top: 5px;
+  color: #212529;
+  &:hover{
+      color: red;
+  }
+`;
